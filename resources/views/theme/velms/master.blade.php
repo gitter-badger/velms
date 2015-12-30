3287b7b8a75e7d3f348872584f24793fd86a4794
Derek Bourgeois <derek@ibourgeois.com>
2015-12-30 08:37:10 -0500

    Update theme

--- resources/views/theme/velms/master.blade.php
+++ resources/views/theme/velms/master.blade.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"/>
-
-		<title>HOME | V-ELMS</title>
-
+  	<head>
+    	<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 		<meta content="" name="description"/>
 		<meta content="" name="author"/>
 
+    	<title>HOME | V-ELMS</title>
+
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
 
 		<link rel="shortcut icon" href="favicon.ico"/>
-	</head>
-	<body>
+
+    	<!--[if lt IE 9]>
+      		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+  	</head>
+  	<body>
 
 		@include('theme.velms.partial.header')
+
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  	</body>
+</html>
+
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+	<body>
 
 		<div class="page-container">
 
@@ -75,3 +91,4 @@
         </div>
 	</body>
 </html>
+-->
