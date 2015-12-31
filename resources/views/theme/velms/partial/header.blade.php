de092ffe5f21dffce4b35087d8b0248945bf43e6
Derek Bourgeois <derek@ibourgeois.com>
2015-12-30 23:45:57 -0500

    Update velms theme

--- resources/views/theme/velms/partial/header.blade.php
+++ resources/views/theme/velms/partial/header.blade.php
@@ -1,20 +1,22 @@
-<div class="container header">
-    <div class="logo pull-left">
-        <a href="/">
-            <img src="/assets/img/logo.png" alt="V-ELMS" style="margin-top: -3px;">
-        </a>
-    </div>
-    <div class="auth nav navbar-nav pull-right">
-        <a href="/auth/login" class="btn btn-primary">Login</a>
-        <a href="/auth/register" class="btn btn-primary">Register</a>
-    </div>
-    <div class="toggle-nav pull-right">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
+<div class="container-fluid header">
+    <div class="container">
+        <div class="logo pull-left">
+            <a href="/">
+                <img src="/assets/img/logo.png" alt="V-ELMS" style="margin-top: -3px;">
+            </a>
+        </div>
+        <div class="auth nav navbar-nav pull-right">
+            <a href="/auth/login" class="btn btn-primary">Login</a>
+            <a href="/auth/register" class="btn btn-primary">Register</a>
+        </div>
+        <div class="toggle-nav pull-right">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
     </div>
 </div>
 <div class="container-fluid navigation">
