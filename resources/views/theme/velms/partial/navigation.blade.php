6a59e351b72cb7fb39174f725bc038972226d80d
Derek Bourgeois <derek@ibourgeois.com>
2015-12-31 00:07:25 -0500

    Update velms theme

--- resources/views/theme/velms/partial/navigation.blade.php
+++ resources/views/theme/velms/partial/navigation.blade.php
@@ -42,7 +42,6 @@
     <li>
         <a href="/contact-us">Contact Us</a>
     </li>
-    <li role="separator" class="divider visible-xs visible-sm"></li>
     <li>
         <a href="/auth/login" class="visible-xs visible-sm">Login</a>
     </li>
