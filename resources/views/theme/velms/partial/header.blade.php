a6c17aa22b73b6d49b3c8c954d1ea347afa01f80
Derek Bourgeois <derek@ibourgeois.com>
2015-12-31 00:09:44 -0500

    Update velms theme

--- resources/views/theme/velms/partial/header.blade.php
+++ resources/views/theme/velms/partial/header.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid header">
     <div class="container">
-        <div class="logo pull-left">
+        <div class="logo">
             <a href="/">
                 <img src="/assets/img/logo.png" alt="V-ELMS" style="margin-top: -3px;">
             </a>
