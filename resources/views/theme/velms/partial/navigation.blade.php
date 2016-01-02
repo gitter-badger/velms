2226235ba0ee866c0fc08a34bda5dd79094bcc07
Derek Bourgeois <derek@ibourgeois.com>
2016-01-02 14:26:09 -0500

    another double navbar test

--- resources/views/theme/velms/partial/navigation.blade.php
+++ resources/views/theme/velms/partial/navigation.blade.php
@@ -2,55 +2,27 @@
     <div id="navbar" class="container collapse navbar-collapse">
         <div class="row">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/campus">Campus</a>
-                </li>
-                <li>
-                    <a href="/why-we-serve">Why We Serve</a>
-                </li>
-                <li>
-                    <a href="/how-we-serve">How We Serve</a>
-                </li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/campus">Campus</a></li>
+                <li><a href="/why-we-serve">Why We Serve</a></li>
+                <li><a href="/how-we-serve">How We Serve</a></li>
                 <li class="menu-dropdown classic-menu-dropdown ">
                     <a data-hover="classic-dropdown" data-close-others="true" data-toggle="dropdown" href="/how-you-can-help">
                         How You Can Help <i class="fa fa-angle-down"></i>
                     </a>
                     <ul class="dropdown-menu pull-left">
-                        <li>
-                            <a href="/mentorship">Mentorship</a>
-                        </li>
-                        <li>
-                            <a href="/mentor-contract">Mentor Contract</a>
-                        </li>
-                        <li>
-                            <a href="/volunteer-opportunities">Volunteer Opportunities</a>
-                        </li>
-                        <li>
-                            <a href="/donations">Donations</a>
-                        </li>
+                        <li><a href="/mentorship">Mentorship</a></li>
+                        <li><a href="/mentor-contract">Mentor Contract</a></li>
+                        <li><a href="/volunteer-opportunities">Volunteer Opportunities</a></li>
+                        <li><a href="/donations">Donations</a></li>
                     </ul>
                 </li>
-                <li>
-                    <a href="/our-partners">Our Partners</a>
-                </li>
-                <li>
-                    <a href="/our-board">Our Board</a>
-                </li>
-                <li>
-                    <a href="/program-curricula">Program Curricula</a>
-                </li>
-                <li>
-                    <a href="/contact-us">Contact Us</a>
-                </li>
-                <li>
-                    <a href="/auth/login" class="visible-xs visible-sm">Login</a>
-                </li>
-                <li>
-                    <a href="/auth/register" class="visible-xs visible-sm">Register</a>
-                </li>
+                <li><a href="/our-partners">Our Partners</a></li>
+                <li><a href="/our-board">Our Board</a></li>
+                <li><a href="/program-curricula">Program Curricula</a></li>
+                <li><a href="/contact-us">Contact Us</a></li>
+                <li><a href="/auth/login" class="visible-xs visible-sm">Login</a></li>
+                <li><a href="/auth/register" class="visible-xs visible-sm">Register</a></li>
             </ul>
         </div>
     </div>
@@ -68,9 +40,27 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/campus">Campus</a></li>
+                <li><a href="/why-we-serve">Why We Serve</a></li>
+                <li><a href="/how-we-serve">How We Serve</a></li>
+                <li class="menu-dropdown classic-menu-dropdown ">
+                    <a data-hover="classic-dropdown" data-close-others="true" data-toggle="dropdown" href="/how-you-can-help">
+                        How You Can Help <i class="fa fa-angle-down"></i>
+                    </a>
+                    <ul class="dropdown-menu pull-left">
+                        <li><a href="/mentorship">Mentorship</a></li>
+                        <li><a href="/mentor-contract">Mentor Contract</a></li>
+                        <li><a href="/volunteer-opportunities">Volunteer Opportunities</a></li>
+                        <li><a href="/donations">Donations</a></li>
+                    </ul>
+                </li>
+                <li><a href="/our-partners">Our Partners</a></li>
+                <li><a href="/our-board">Our Board</a></li>
+                <li><a href="/program-curricula">Program Curricula</a></li>
+                <li><a href="/contact-us">Contact Us</a></li>
+                <li><a href="/auth/login" class="visible-xs visible-sm">Login</a></li>
+                <li><a href="/auth/register" class="visible-xs visible-sm">Register</a></li>
             </ul>
         </div>
     </div>
