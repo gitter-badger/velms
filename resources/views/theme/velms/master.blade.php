a32bf44b5ad6992dcc47072a2bf2a724b35c3bab
Derek Bourgeois <derek@ibourgeois.com>
2016-01-01 22:41:10 -0500

    remove double navbar

--- resources/views/theme/velms/master.blade.php
+++ resources/views/theme/velms/master.blade.php
@@ -28,7 +28,7 @@
 		@include('theme.velms.partial.header')
 
         @include('theme.velms.partial.navigation')
-        @include('theme.velms.partial.navigation')
+
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   	</body>
