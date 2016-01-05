7eda45c915ea18187e9d591c54925757cf4f5ed9
Derek Bourgeois <derek@ibourgeois.com>
2016-01-05 15:07:35 -0500

    add Admin Policy

---
+++ app/Policies/AdminPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Velms\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AdminPolicy
+{
+    use HandlesAuthorization;
+
+    public function __construct()
+    {
+
+    }
+
+    public function before($user)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+}
